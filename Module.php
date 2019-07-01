4dec98a2a75157ecfb7a7cec769e0a0bdaa85ba0
Jim Safley <jimsafley@gmail.com>
2019-07-01 14:49:26 -0400

    Add initial JS

--- Module.php
+++ Module.php
@@ -2,7 +2,20 @@
 namespace LinkedDataDisplay;
 
 use Omeka\Module\AbstractModule;
+use Zend\EventManager\Event;
+use Zend\EventManager\SharedEventManagerInterface;
 
 class Module extends AbstractModule
 {
+    public function attachListeners(SharedEventManagerInterface $sharedEventManager)
+    {
+        $sharedEventManager->attach(
+            'Omeka\Controller\Site\Item',
+            'view.show.before',
+            function (Event $event) {
+                $view = $event->getTarget();
+                $view->headScript()->prependFile($view->assetUrl('js/linked-data-display.js', 'LinkedDataDisplay'));
+            }
+        );
+    }
 }
