a7e029b9ace9b4e5438a8e8aa073ab442bdab220
Jim Safley <jimsafley@gmail.com>
2019-07-01 23:07:55 -0400

    Better toggling

--- asset/js/linked-data-display.js
+++ asset/js/linked-data-display.js
@@ -26,20 +26,38 @@ document.addEventListener('DOMContentLoaded', function(event) {
     for (let uriValue of document.getElementsByClassName('uri-value-link')) {
         LinkedDataDisplay.services.forEach(function(service) {
             if (service.isMatch(uriValue.href)) {
-                // The fetch button.
-                const button = document.createElement('button');
-                button.className = 'linked-data-display-button';
-                button.innerHTML = 'more';
-                button.onclick = function() {
-                    LinkedDataDisplay.display(service, uriValue.href, container);
-                    this.remove();
-                };
 
                 // The data markup container.
                 const container = document.createElement('div');
                 container.className = 'linked-data-display';
 
-                uriValue.parentNode.appendChild(button);
+                // The toggle button.
+                const toggleButton = document.createElement('button');
+                toggleButton.className = 'linked-data-display-more-less';
+                toggleButton.innerHTML = '-';
+                toggleButton.style.display = 'none';
+                toggleButton.onclick = function() {
+                    if ('none' === container.style.display) {
+                        container.style.display = 'block';
+                        this.innerHTML = '-';
+                    } else {
+                        container.style.display = 'none';
+                        this.innerHTML = '+';
+                    }
+                };
+
+                // The fetch button.
+                const fetchButton = document.createElement('button');
+                fetchButton.className = 'linked-data-display-fetch';
+                fetchButton.innerHTML = '+';
+                fetchButton.onclick = function() {
+                    LinkedDataDisplay.display(service, uriValue.href, container);
+                    toggleButton.style.display = 'inline';
+                    this.remove();
+                };
+
+                uriValue.parentNode.appendChild(fetchButton);
+                uriValue.parentNode.appendChild(toggleButton);
                 uriValue.parentNode.appendChild(container);
 
                 return false;
