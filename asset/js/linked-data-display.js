be55e5bb60d8cead4b5ad1ee800cecf587263925
Jim Safley <jimsafley@gmail.com>
2019-07-02 22:31:57 -0400

    Refactor

--- asset/js/linked-data-display.js
+++ asset/js/linked-data-display.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const LinkedDataDisplay = {
-    services : [],
+    services: {},
     /**
      * Add a linked data service object.
      *
@@ -20,71 +20,81 @@ const LinkedDataDisplay = {
         this.services[service.getName()] = service;
     },
     /**
+     * Get a service object by URI.
+     *
+     * @param {string} uri The URI
+     * @return {object|false} Returns false if no service matches
+     */
+    getServiceByUri: function(uri) {
+        for (let serviceName in this.services) {
+            const service = this.services[serviceName];
+            if (service.isMatch(uri)) {
+                return service;
+            }
+        }
+        return false;
+    },
+    /**
      * Display linked data.
      *
-     * @param {object} service The linked data service object
      * @param {string} uri The URI
      * @param {object} container The container element
      */
-    display: function(service, uri, container) {
-        return fetch(service.getEndpoint(uri))
-            .then(function(response) {
-                if (!response.ok) {
-                    throw new Error(`HTTP Error ${response.status}`);
-                }
-                return response.text();
-            })
-            .then(function(text) {
-                container.innerHTML = service.getMarkup(uri, text);
-            })
-            .catch(function(error) {
-                console.log(`Error in service "${service.getName()}" using URI "${uri}": ${error.message}`);
-            });
+    display: function(uri, container) {
+        const service = this.getServiceByUri(uri);
+        if (service) {
+            fetch(service.getEndpoint(uri))
+                .then(function(response) {
+                    if (!response.ok) {
+                        throw new Error(`HTTP Error ${response.status}`);
+                    }
+                    return response.text();
+                })
+                .then(function(text) {
+                    container.innerHTML = service.getMarkup(uri, text);
+                })
+                .catch(function(error) {
+                    console.log(`Error in service "${service.getName()}" using URI "${uri}": ${error.message}`);
+                });
+        }
     }
 };
 
 document.addEventListener('DOMContentLoaded', function(event) {
     for (let uriValue of document.getElementsByClassName('uri-value-link')) {
-        for (let serviceName in LinkedDataDisplay.services) {
-            const service = LinkedDataDisplay.services[serviceName];
-            if (service.isMatch(uriValue.href)) {
+        if (LinkedDataDisplay.getServiceByUri(uriValue.href)) {
+            // The data markup container.
+            const container = document.createElement('div');
+            container.className = 'linked-data-display';
 
-                // The data markup container.
-                const container = document.createElement('div');
-                container.className = 'linked-data-display';
+            // The toggle button.
+            const toggleButton = document.createElement('button');
+            toggleButton.className = 'linked-data-display-more-less';
+            toggleButton.innerHTML = '-';
+            toggleButton.style.display = 'none';
+            toggleButton.onclick = function() {
+                if ('none' === container.style.display) {
+                    container.style.display = 'block';
+                    this.innerHTML = '-';
+                } else {
+                    container.style.display = 'none';
+                    this.innerHTML = '+';
+                }
+            };
 
-                // The toggle button.
-                const toggleButton = document.createElement('button');
-                toggleButton.className = 'linked-data-display-more-less';
-                toggleButton.innerHTML = '-';
-                toggleButton.style.display = 'none';
-                toggleButton.onclick = function() {
-                    if ('none' === container.style.display) {
-                        container.style.display = 'block';
-                        this.innerHTML = '-';
-                    } else {
-                        container.style.display = 'none';
-                        this.innerHTML = '+';
-                    }
-                };
+            // The fetch button.
+            const fetchButton = document.createElement('button');
+            fetchButton.className = 'linked-data-display-fetch';
+            fetchButton.innerHTML = '+';
+            fetchButton.onclick = function() {
+                LinkedDataDisplay.display(uriValue.href, container);
+                toggleButton.style.display = 'inline';
+                this.remove();
+            };
 
-                // The fetch button.
-                const fetchButton = document.createElement('button');
-                fetchButton.className = 'linked-data-display-fetch';
-                fetchButton.innerHTML = '+';
-                fetchButton.onclick = function() {
-                    LinkedDataDisplay.display(service, uriValue.href, container);
-                    toggleButton.style.display = 'inline';
-                    this.remove();
-                };
-
-                uriValue.parentNode.appendChild(fetchButton);
-                uriValue.parentNode.appendChild(toggleButton);
-                uriValue.parentNode.appendChild(container);
-
-                // Found a matching service. Move on to next uriValue.
-                break;
-            }
+            uriValue.parentNode.appendChild(fetchButton);
+            uriValue.parentNode.appendChild(toggleButton);
+            uriValue.parentNode.appendChild(container);
         }
     }
 });
