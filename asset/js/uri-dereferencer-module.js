8131abf35acd0e4874835930dbc1887f93ad9102
Jim Safley <jimsafley@gmail.com>
2023-03-01 20:57:18 -0500

    Move toggles to left (fix #9, close #8)

--- asset/js/uri-dereferencer-module.js
+++ asset/js/uri-dereferencer-module.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
         const toggleButton = document.createElement('a');
         toggleButton.className = 'uri-dereferencer-toggle';
         toggleButton.href = '#';
-        toggleButton.innerHTML = '-';
+        toggleButton.innerHTML = '[–]';
         toggleButton.setAttribute('aria-controls', containerId);
         toggleButton.setAttribute('aria-expanded', 'false');
         toggleButton.style.display = 'none';
@@ -24,11 +24,11 @@ document.addEventListener('DOMContentLoaded', function(event) {
             if ('none' === container.style.display) {
                 container.style.display = 'inline-flex';
                 container.focus();
-                this.innerHTML = '-';
+                this.innerHTML = '[–]';
                 this.setAttribute('aria-expanded', 'true');
             } else {
                 container.style.display = 'none';
-                this.innerHTML = '+';
+                this.innerHTML = '[+]';
                 this.setAttribute('aria-expanded', 'false');
             }
         };
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
         const fetchButton = document.createElement('a');
         fetchButton.className = 'uri-dereferencer-fetch';
         fetchButton.href = '#';
-        fetchButton.innerHTML = '+';
+        fetchButton.innerHTML = '[+]';
         loading = document.createElement('i');
         loading.className = 'loading';
         fetchButton.onclick = async function(e) {
@@ -45,13 +45,13 @@ document.addEventListener('DOMContentLoaded', function(event) {
             container.innerHTML = loading.outerHTML;
             container.innerHTML = await UriDereferencer.dereference(uriValue.href);
             container.focus();
-            this.remove();
+            this.style.display = 'none';
             toggleButton.style.display = 'inline-flex';
             toggleButton.setAttribute('aria-expanded', 'true');
         };
 
-        uriValue.parentNode.appendChild(fetchButton);
-        uriValue.parentNode.appendChild(toggleButton);
-        uriValue.parentNode.appendChild(container);
+        uriValue.parentNode.prepend(fetchButton);
+        uriValue.parentNode.prepend(toggleButton);
+        uriValue.parentNode.append(container);
     }
 });
